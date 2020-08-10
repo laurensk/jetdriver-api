4d688ff604863e86ba4979393c2e288aadc79747
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-10 21:39:17 +0200

    router added

--- src/router.js
+++ src/router.js
@@ -1,4 +1,15 @@
+const cars = require('./routes/cars');
+const companions = require('./routes/companions');
+const entries = require('./routes/entries');
+const properties = require('./routes/properties');
+const user = require('./routes/user');
 
 const router = require('express').Router();
+
+router.use('/cars', cars);
+router.use('/companions', companions);
+router.use('/entries', entries);
+router.use('/properties', properties);
+router.use('/user', user);
 
 export default router;
