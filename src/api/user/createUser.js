a1f198e3aa70da0e97b18d166b9176169a7ef355
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-14 20:41:56 +0200

    api callback logic

--- src/api/user/createUser.js
+++ src/api/user/createUser.js
@@ -1,5 +1,6 @@
 'use strict';
 
-module.exports = function sendError(res, statusCode, message) {
-    res.status(statusCode).send({ error: true, statusCode: statusCode, message: message });
+module.exports = function createUser(email, password, completion) {
+    const uuid = email + password;
+    completion(uuid);
 }
