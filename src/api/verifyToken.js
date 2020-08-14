5fb7a2609e95f06eb276ff2246140b3a63ee3fc5
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-14 19:27:19 +0200

    updated sql helpers

--- src/api/verifyToken.js
+++ src/api/verifyToken.js
@@ -14,7 +14,7 @@ module.exports = function verifyToken(req, res, next) {
         const bearerToken = bearer[1];
         req.token = bearerToken;
 
-        sql.query('SELECT * FROM JDTokens WHERE tokToken = ' + mysql.escape(bearerToken) + ' AND tokValid = TRUE', function (err, rows, fields) {
+        sql().query('SELECT * FROM JDTokens WHERE tokToken = ' + mysql.escape(bearerToken) + ' AND tokValid = TRUE', function (err, rows, fields) {
             if (err) throw err;
             if (rows.length === 1) {
                 console.log('User authenticated');
--- src/db.js
+++ src/db.js
@@ -1,18 +1,59 @@
 'use strict';
 
-var mysql = require('mysql');
-require('dotenv/config');
+// var mysql = require('mysql');
+// require('dotenv/config');
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-});
+// var connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 3306,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+// });
 
-connection.connect(function (err) {
-    if (err) throw err;
-});
+// connection.connect(function (err) {
+//     if (err) throw err;
+// });
 
-module.exports = connection;
+// module.exports = connection;
+
+module.exports = () => {
+
+    var mysql = require('mysql');
+    require('dotenv/config');
+
+    var db_config = {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+    };
+
+    var connection;
+
+    function handleDisconnect() {
+        connection = mysql.createConnection(db_config); // Recreate the connection, since
+
+        connection.connect(function (err) {
+            if (err) {
+                console.log('error when connecting to db:', err);
+                setTimeout(handleDisconnect, 2000);
+            }
+        });
+
+        connection.on('error', function (err) {
+            console.log('db error', err);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+                handleDisconnect();
+            } else {
+                throw err;
+            }
+        });
+    }
+
+    handleDisconnect();
+
+    return connection;
+
+}
