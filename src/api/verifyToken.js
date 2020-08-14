24fcdfa092a9816d6c62a1dbad207bcc6d2455b2
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-14 13:50:26 +0200

    token verification

--- src/api/verifyToken.js
+++ src/api/verifyToken.js
@@ -1,25 +1,28 @@
 'use strict';
 
 const sendError = require("./sendError");
+const mysql = require('mysql');
 const sql = require("../db");
 
+
 module.exports = function verifyToken(req, res, next) {
+
     const bearerHeader = req.headers['authorization'];
 
     if (bearerHeader) {
         const bearer = bearerHeader.split(' ');
         const bearerToken = bearer[1];
         req.token = bearerToken;
-        next();
-    } else {
-        sendError(res, 403, 'unauthorized');
-    }
-    sql.query('SELECT * FROM JDTokens WHERE ', function (err, rows, fields) {
-        if (err) throw err;
-        console.log('The solution is: ', rows[0].solution);
-    });
-    if (false) {
-        next();
+
+        sql.query('SELECT * FROM JDTokens WHERE tokToken = ' + mysql.escape(bearerToken) + ' AND tokValid = TRUE', function (err, rows, fields) {
+            if (err) throw err;
+            if (rows.length === 1) {
+                next();
+            } else {
+                sendError(res, 403, 'unauthorized');
+            }
+        });
+
     } else {
         sendError(res, 403, 'unauthorized');
     }
