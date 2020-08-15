c2db2c2ef3b715b237bc3440e0c9a86c310d7930
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-15 23:22:20 +0200

    node rewrite to module based app

--- src/api/user/createUser.js
+++ src/api/user/createUser.js
@@ -2,14 +2,27 @@
 
 const mysql = require('mysql');
 const sql = require("../../db");
-const { Error } = require('../helpers/errorHandling');
+const { Error, sendError } = require('../helpers/errorHandling');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User.model');
 
 module.exports = function createUser(email, password, callback) {
 
-    sql.query('SELECT useEmail from JDUsers WHERE useEmail = ' + mysql.escape(email), (err, rows) => {
+    sql.query('SELECT useEmail FROM JDUsers WHERE useEmail = ?', [email], async (err, rows) => {
         if (err) return callback(Error.unknownError, null);
         if (rows.length > 0) return callback(Error.emailExists, null);
 
+        const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        sql.query(`INSERT INTO JDUsers (useId, useEmail, usePassword, useSalt) VALUES (UUID(), ?, ?, ?)`, [email, hashPassword, salt], (err) => {
+            if (err) return callback(Error.unknownError, null);
+            sql.query('SELECT * FROM JDUsers WHERE useEmail = ?', [email], (err, rows) => {
+                const dbUser = rows[0];
+                const user = new User({ uuid: dbUser.useId, email: dbUser.useEmail })
+                callback(user, null);
+            })
+        });
 
 
     })
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -4,13 +4,11 @@ class User {
     constructor
         (
             uuid,
-            name,
             email,
             password,
             salt
         ) {
         this.uuid = uuid;
-        this.name = name;
         this.email = email;
         this.password = password;
         this.salt = salt;
