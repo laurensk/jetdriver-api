38edb8bd6275a16c9c578257be44a587d8879696
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-16 13:18:00 +0200

    login user

--- src/api/user/loginUser.js
+++ src/api/user/loginUser.js
@@ -1,5 +1,27 @@
 'use strict';
 
+const sql = require("../../db");
+const { Error } = require('../helpers/errorHandling');
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User.model');
+const jwt = require('jsonwebtoken');
+
 module.exports = function loginUser(email, password, callback) {
-    console.log("login user");
+
+    sql.query('SELECT useId, useEmail, useName, usePassword FROM JDUsers WHERE useEmail = ?', [email], async (err, rows) => {
+        if (err) return callback(Error.unknownError, null);
+        if (!rows.length == 1) return callback(Error.loginFailed, null);
+
+        const dbUser = rows[0];
+        const comparePassword = await bcrypt.compare(password, dbUser.usePassword);
+        if (!comparePassword) return callback(Error.loginFailed);
+
+        const token = jwt.sign({ uuid: dbUser.useId }, process.env.TOKEN_SECRET);
+
+        sql.query('INSERT INTO JDTokens (tokId, tokToken, tokUseId, tokValid) VALUES (UUID(), ?, ?, TRUE)', [token, dbUser.useId], (err) => {
+            if (err) return callback(Error.unknownError, null);
+            const user = new User(dbUser.useId, dbUser.useEmail, dbUser.useName);
+            callback(null, user, token);
+        });
+    });
 }
