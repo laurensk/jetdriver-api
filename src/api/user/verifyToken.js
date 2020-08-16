74b44fa52149dcd7c167e691d8ede70208128304
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-16 12:32:59 +0200

    refactored verifytoken

--- src/api/user/verifyToken.js
+++ src/api/user/verifyToken.js
@@ -8,7 +8,7 @@ module.exports = function verifyToken(req, res, next) {
 
     const bearerHeader = req.headers['authorization'];
 
-    if (!bearerHeader) return sendError(Error.unauthenticated);
+    if (!bearerHeader) return sendError(res, Error.unauthenticated);
 
     const bearer = bearerHeader.split(' ');
     const bearerToken = bearer[1];
@@ -16,13 +16,11 @@ module.exports = function verifyToken(req, res, next) {
 
     sql.query('SELECT tokUseId FROM JDTokens WHERE tokToken = ? AND tokValid = TRUE', [bearerToken], (err, rows) => {
         console.log(rows);
-        if (err) return sendError(Error.unknownError);
-        if (rows.length === 1) {
-            const uuid = rows[0].tokUseId;
-            req.body.uuid = uuid;
-            next();
-        } else {
-            sendError(res, Error.unauthenticated);
-        }
+        if (err) return sendError(res, Error.unknownError);
+        if (!rows.length === 1) return sendError(res, Error.unauthenticated);
+
+        const uuid = rows[0].tokUseId;
+        req.body.uuid = uuid;
+        next();
     });
 }
