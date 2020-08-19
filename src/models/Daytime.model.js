dc0d5a2ac51e2dbd1d9d55237934a5213f9b69f9
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-19 22:12:26 +0200

    added daytime

---
+++ src/models/Daytime.model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Daytime {
+    constructor
+        (
+            id,
+            daytime
+        ) {
+        this.id = id;
+        this.daytime = daytime;
+    }
+}
+
+module.exports = Daytime;
