e6ff363decf8b30b462cc0b86c66b41db2bd32a7
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-08-19 17:31:16 +0200

    companion routes

--- src/routes/companions.js
+++ src/routes/companions.js
@@ -4,15 +4,52 @@ const verifyToken = require('../api/user/verifyToken');
 const router = require('express').Router();
 
 router.route('/').get(verifyToken, (req, res) => {
-    res.send('companions');
+
+    getCompanionsForUser(req.body.uuid, (error, cars) => {
+        if (error) return sendError(res, error);
+        res.json({
+            cars: cars
+        });
+    });
 });
 
 router.route('/').post(verifyToken, (req, res) => {
-    res.send('create companion');
+
+    const validation = Joi.object({
+        uuid: Joi.string().required(),
+        name: Joi.string().required()
+    });
+    const { error } = validation.validate(req.body);
+    if (error) return sendError(res, Error.validationError);
+
+    createCompanion(req.body.uuid, req.body.name, (error, car) => {
+        if (error) return sendError(res, error);
+        res.json({
+            companion: companion
+        });
+    });
 });
 
-router.route('/:uuid').get(verifyToken, (req, res) => {
-    res.send('will get the companion with id ' + req.params.uuid);
+router.route('/:comId').get(verifyToken, (req, res) => {
+
+    const comId = req.params.comId;
+    getCompanionWithId(req.body.uuid, comId, (error, car) => {
+        if (error) return sendError(res, error);
+        res.json({
+            companion: companion
+        });
+    });
+});
+
+router.route('/:comId').delete(verifyToken, (req, res) => {
+
+    const comId = req.params.comId;
+    deleteCompanion(req.body.uuid, comId, (error, deletion) => {
+        if (error) return sendError(res, error);
+        res.json({
+            delete: deletion
+        });
+    });
 });
 
 module.exports = router;
